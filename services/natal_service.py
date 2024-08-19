d77d51757ee671df299dbf972cdf284020118c11
Nikolay Pushkin <adwedelf@gmail.com>
2024-08-20 01:27:28 +0700

    fix: encoding error

--- services/natal_service.py
+++ services/natal_service.py
@@ -20,22 +20,22 @@ def get_natal_description(data: NatalData):
     for object in natal.objects.values():
         arr.append([object.name, object.sign.number, object.house.number])
 
-    with open('./data/Houses.json', 'r') as f:
-        Houses = json.load(f)
-    with open('./data/Signs.json', 'r') as f:
-        Signs = json.load(f)
+    with open('./data/Houses.json', 'r', encoding='utf-8') as f:
+        houses = json.load(f)
+    with open('./data/Signs.json', 'r', encoding='utf-8') as f:
+        signs = json.load(f)
 
     result = {}
     signs_description = []
     houses_description = []
-    houses_keys = Houses.keys()
-    signs_keys = Signs.keys()
+    houses_keys = houses.keys()
+    signs_keys = signs.keys()
     for item in arr:
         planet_name = item[0]
         if planet_name in signs_keys:
-            signs_description.append(Signs[planet_name][item[1] - 1])
+            signs_description.append(signs[planet_name][item[1] - 1])
         if planet_name in houses_keys:
-            houses_description.append(Houses[planet_name][item[2] - 1])
+            houses_description.append(houses[planet_name][item[2] - 1])
 
     result["Планеты и точки в Знаках"] = signs_description
     result["Планеты и Точки в Домах"] = houses_description
